cc5897262919f4111b37a80d49341ce101dc5bb5
3-DD13 <eddiejuarez521@gmail.com>
2026-06-27 03:05:44 -0700

    Trying to add home page and linking it with React

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from backend.routes import api_routes
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 CORS(app)
 
 app.register_blueprint(api_routes)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react(path):
+  if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+    return send_from_directory(app.static_folder, path)
+  else:
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
