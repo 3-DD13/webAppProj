ccc1a4bf74c9bf0c08aeb993a76f1230a1362f35
3-DD13 <eddiejuarez521@gmail.com>
2026-06-27 03:21:20 -0700

    working on making github pushes go to web-app also

--- backend/routes.py
+++ backend/routes.py
@@ -1,9 +1,27 @@
+import subprocess
 from flask import Blueprint, jsonify
 
 api_routes = Blueprint('api', __name__)
+
+# Home route defined in app.py
 
 @api_routes.route('/api/test', methods=['GET'])
 def test_route():
   return jsonify({
     "message": "Backend testing w/ Flask, remove me later"
   })
+
+# gitHub pushes should go directly to the web-app (more for convenience)
+@api_routes.route('/api/update-server', methods=['POST'])
+def update_server():
+  try:
+    commands = (
+      "cd /home/hjuarez/webAppProj && "
+      "git fetch --all &&"
+      "git reset --hard origin/main"
+    )
+    subprocess.check_call(commands, shell=True)
+    return jsonify({"message": "gitHub push recieved"})
+
+  except subprocess.CalledProcessError as e:
+    return jsonify({"error": str(e)}), 500
