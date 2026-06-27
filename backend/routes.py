c0b58049f7da16d7226d9fcb9ab1caa85fabe29c
3-DD13 <eddiejuarez521@gmail.com>
2026-06-27 01:07:46 -0700

    Bones of the website

---
+++ backend/routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, jsonify
+
+api_routes = Blueprint('api', __name__)
+
+@api_routes.route('api/test', __name__)
+def test_route():
+  return jsonify({
+    "message": "Backend testing w/ Flask, remove me later"
+  })
