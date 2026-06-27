5735d46c3e060dbfc8f4e683b3453391967b71f9
3-DD13 <eddiejuarez521@gmail.com>
2026-06-27 01:40:18 -0700

    Fixing minor typo

--- backend/routes.py
+++ backend/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 api_routes = Blueprint('api', __name__)
 
-@api_routes.route('api/test', __name__)
+@api_routes.route('api/test', methods=['GET'])
 def test_route():
   return jsonify({
     "message": "Backend testing w/ Flask, remove me later"
